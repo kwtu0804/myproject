ef1f5b7aaae2e4f3f1df6f6f0ce05cf4f90d8c24
kwtu0804 <kwtu0804@gmail.com>
2019-01-27 01:13:56 +0800

    initial commit

---
+++ src/main/kotlin/com/jesper/Hello.kt
@@ -0,0 +1,14 @@
+package com.jesper
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin")
+    //Human().hello()
+    val h=Human()
+    h.hello()
+}
+
+class Human{
+    fun hello(){
+        println("Hello Kotlin")
+    }
+}
